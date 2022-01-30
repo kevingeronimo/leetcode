b3ae6fb1fdc7e9a28ba75545024f85e465040aa7
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-01-29 20:17:56 -0500

    Time: 4 ms (82.35%), Space: 2.4 MB (5.88%) - LeetHub

---
+++ 704-binary-search/704-binary-search.rs
@@ -0,0 +1,25 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut min: usize = 0;
+        let mut max: usize = nums.len();
+
+        loop {
+            // rust will floor if the left value is not a float
+            let mid = (min + max) / 2;
+
+            // check first guess
+            if target == nums[mid] {
+                break mid as i32;
+            // min = mid when integer not found
+            } else if min == mid {
+                break -1;
+            // removes right half
+            } else if target < nums[mid] {
+                max = mid
+            // removes left half
+            } else {
+                min = mid
+            };
+        }
+    }
+}
