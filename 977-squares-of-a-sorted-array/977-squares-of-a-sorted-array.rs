d150298b31c30ac2295d6cc4c30a3186af48c15c
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-02-02 23:56:02 -0500

    Time: 28 ms (13.24%), Space: 2.4 MB (15.33%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.rs
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.rs
@@ -1,7 +1,27 @@
-impl Solution {
+impl Solution { 
     pub fn sorted_squares(nums: Vec<i32>) -> Vec<i32> {
-        let mut squares: Vec<i32> = nums.iter().map(|x| x.pow(2)).collect();
-        squares.sort_unstable();
-        squares
+        let n = nums.len() - 1;
+        let mut result = Vec::new();
+        let (mut beg, mut end, mut i) = (0, n, n);
+
+        loop {
+            if nums[beg].pow(2) > nums[end].pow(2) {
+                result.insert(0, nums[beg].pow(2));
+                beg += 1
+            } else {
+                result.insert(0, nums[end].pow(2));
+                if end != 0 {
+                    end -= 1
+                }
+            }
+
+            if i != 0 {
+                i -= 1
+            } else {
+                break
+            }
+        }
+
+        result
     }
 }
