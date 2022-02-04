fe863a22e6149fcb7d2903fd79244293d37d5e7f
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-02-03 19:58:44 -0500

    Time: 3 ms (32.78%), Space: 2.2 MB (64.44%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.rs
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.rs
@@ -1,18 +1,21 @@
 impl Solution {
     pub fn two_sum(numbers: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut pivot = 0;
-        let mut pointer = pivot + 1;
+        let mut low = 0;
+        let mut high = numbers.len() - 1;
+        let mut output = Vec::new();
 
-        loop {
-            if numbers[pivot] + numbers[pointer] == target {
-                break vec![pivot as i32 + 1, pointer as i32 + 1]
-            } else if pointer == numbers.len() - 1 {
-                pivot += 1;
-                pointer = pivot + 1;
-            }
-            else {
-                pointer += 1
+        while low < high {
+            if numbers[low] + numbers[high] == target {
+                output.push(low as i32 + 1);
+                output.push(high as i32 + 1);
+                break
+            } else if numbers[low] + numbers[high] < target {
+                low += 1
+            } else {
+                high -= 1
             };
         }
+
+        output
     }
 }
