cc48dcea0cbccf2a50a7967dc791eba8c74e38c0
Kevin Geronimo <96751432+kevingeronimo@users.noreply.github.com>
2022-02-04 23:50:48 -0500

    Time: 3 ms (65.79%), Space: 2.1 MB (90.35%) - LeetHub

---
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.rs
@@ -0,0 +1,9 @@
+use std::iter::FromIterator;
+
+impl Solution {
+    pub fn reverse_words(s: String) -> String {
+        s.split(' ')
+            .map(|word| String::from_iter(word.chars().rev()))
+            .reduce(|string, word| string + " " + &word).unwrap()
+    }
+}
